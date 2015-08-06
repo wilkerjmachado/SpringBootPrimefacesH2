e6ad5789559928f8e4028b7a22d3a00460ff13c4
Wilker Machado <wilkerjmachado@gmail.com>
2015-08-05 22:18:28 -0300

    finalizado

--- src/main/java/org/enquete/app/controller/RespostaController.java
+++ src/main/java/org/enquete/app/controller/RespostaController.java
@@ -1,5 +1,7 @@
 package org.enquete.app.controller;
 
+import javax.faces.application.FacesMessage;
+
 import org.enquete.app.dominio.Pergunta;
 import org.enquete.app.dominio.Resposta;
 import org.enquete.app.formulario.RespostaFormulario;
@@ -8,21 +10,29 @@ import org.enquete.framework.controller.GenericController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 @Scope("request")
-public class RespostaController extends GenericController<Resposta, RespostaFormulario, RespostaService>{
+public class RespostaController extends GenericController<Resposta, RespostaFormulario, RespostaService> {
 
 	@Autowired
 	public RespostaController(RespostaFormulario formulario, RespostaService service) {
 		super(formulario, service);
 	}
-	
+
 	public void salvar(Pergunta pergunta) {
-		
-		this.getFormulario().getEntidade().setPergunta(pergunta);
-		
-		super.salvar();
+
+		if (!StringUtils.isEmpty(this.getFormulario().getEntidade().getTexto().toString())) {
+
+			this.getFormulario().getEntidade().setPergunta(pergunta);
+
+			super.salvar();
+
+		} else {
+
+			this.mostrarMensagem("Preecha dados obrigatórios!", "Alerta", FacesMessage.SEVERITY_INFO);
+		}
 	}
 
 }
